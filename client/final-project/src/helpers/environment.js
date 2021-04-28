64a84ac2aef9f41b2185bec6cd74da5d666465e5
Mia Casas <miacasas@mias-mbp.attlocal.net>
2021-04-28 09:01:56 -0500

    created

--- client/final-project/src/helpers/environment.js
+++ client/final-project/src/helpers/environment.js
@@ -4,8 +4,8 @@ switch(window.location.hostname){
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:5005';
         break;
-    case 'git.heroku.com/lista-shopping-list-main.git':
-        APIURL = 'https://git.heroku.com/lista-shopping-list-main.git'
+    case 'git.heroku.com/lista-shopping-list-client.git':
+        APIURL = 'https://git.heroku.com/lista-shopping-list-client.git'
 }
 
 export default APIURL;
